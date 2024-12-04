b694400a074623794501e8b0aa4f1e4e35079855
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-12-04 19:14:34 +0300

    Add project files.

---
+++ Interitance_Sealed_Keyword/Personel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interitance_Sealed_Keyword
+{
+    public class Personel  // public den sonra sealed yazsaydım musteri sınıfı miras alamazdı
+    {
+        public int personel_id {  get; set; }
+        public string personel_ismi { get; set; }
+        public string personel_soyismi { get; set; }    
+        public int personel_maas {  get; set; }
+
+        public void personelBilgileri()
+        {
+            Console.WriteLine("Personel id : " + personel_id);
+            Console.WriteLine("Personel ismi : " + personel_ismi);
+            Console.WriteLine("Personel soyisim : " + personel_soyismi);
+            Console.WriteLine("Personel Maaş : " + personel_maas);
+
+        }
+
+
+
+
+    }
+}
---
+++ Interitance_Sealed_Keyword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interitance_Sealed_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri  musteri = new Musteri();
+
+            musteri.personel_id = 1;
+            musteri.personel_ismi = "Pınar";
+            musteri.personel_soyismi = "Altınkaya";
+            musteri.personel_maas = 5000;
+
+            musteri.personelBilgileri();
+
+            Console.ReadLine();
+        }
+    }
+}
